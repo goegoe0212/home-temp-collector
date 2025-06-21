84be75392525827015346d1e79007f6f70e82fb2
goegoe0212 <goegoe0212@outlook.jp>
2025-06-21 11:50:43 +0900

    fix: nature_device_nameフィールドを削除

--- app/settings/config.py
+++ app/settings/config.py
@@ -5,7 +5,6 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     loglevel: str = Field("INFO")
     nature_api_key: str = Field(default="sample_api_key")
-    nature_device_name: str = Field(default="Remo")
     postgresql_host: str = Field(default="192.168.20.102")
     postgresql_port: int = Field(default=5432)
     postgresql_dbname: str = Field(default="postgresdb")
